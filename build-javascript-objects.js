90ce5eb61165627396ba9076e3e78d13c89ab99f
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-10-21 07:54:01 -0300

    Create build-javascript-objects.js

---
+++ build-javascript-objects.js
@@ -0,0 +1,8 @@
+var myDog = {
+    // Only change code below this line
+    name: "Dudu",
+    legs: 4,
+    tails: 1,
+    friends: ["Chiquinho", "Julie"]
+    // Only change code above this line
+};
