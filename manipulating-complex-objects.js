bd8bbba5ea9d128c8d4998610ef8186bf22b47d4
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-11-09 20:56:03 -0300

    Create manipulating-complex-objects.js

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Ednaldo Pereira",
+      "title": "Vale tudo vale nada",
+      "release_year": 2010,
+      "formats": [
+        "CD",
+        "Digital"
+      ],
+      "gold": true
+    }
+  ];
