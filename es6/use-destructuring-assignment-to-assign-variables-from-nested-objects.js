7899ab9444759e3371c53d0c2a6787f393902ad3
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2022-02-01 17:50:01 -0300

    Fix resolution

--- es6/use-destructuring-assignment-to-assign-variables-from-nested-objects.js
+++ es6/use-destructuring-assignment-to-assign-variables-from-nested-objects.js
@@ -1,11 +1,10 @@
-const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-  };
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
   
-  // Only change code below this line
-    
-    const { today: highToday, tomorrow: highTomorrow  } = HIGH_TEMPERATURES;
-  
-  // Only change code above this line
+const { today: {high:highToday, low: lowToday}  } = LOCAL_FORECAST;
+// Only change code above this line
