d283463c954952d13a605537f79d8d8cb065c417
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-02-21 19:44:28 -0300

    Create use-an-array-to-store-a-collection-of-data.js

---
+++ basic-data-structures/use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,17 @@
+let yourArray = [
+    {id:1,
+     name:"juan",
+     admin:true},
+     {id:2,
+     name:"colchete",
+     admin:true},
+     {id:3,
+     name:"ana",
+     admin:false},
+     {id:4,
+     name:"jhon",
+     admin:false},
+     {id:5,
+     name:"chris",
+     admin:false}
+  ]; // Change this line
