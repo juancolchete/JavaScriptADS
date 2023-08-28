9edc3f6f42d47550f1323e05c41f2646ebfc1584
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-08-28 18:11:22 -0300

    refactor: manipulate arrays with pop

--- basic-javascript/manipulate-arrays-with-pop.js
+++ basic-javascript/manipulate-arrays-with-pop.js
@@ -1,5 +1,5 @@
 // Setup
-var myArray = [["John", 23], ["cat", 2]];
+const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
-var removedFromMyArray = myArray.pop();
+const removedFromMyArray = myArray.pop()
