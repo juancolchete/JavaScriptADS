b8ec5aed2895be36026b83c717040ea62d77c095
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-11-19 21:46:38 -0300

    refactor: comparisons the logical and operator

--- basic-javascript/comparisons-with-the-logical-and-operator.js
+++ basic-javascript/comparisons-with-the-logical-and-operator.js
@@ -1,12 +1,12 @@
 function testLogicalAnd(val) {
-    // Only change code below this line
-  
-    if (val <= 50 && val >= 25) {
-        return "Yes";
-    }
-  
-    // Only change code above this line
-    return "No";
+  // Only change code below this line
+
+  if (val >= 25 && val <= 50) {
+    return "Yes";
   }
-  
-  testLogicalAnd(10);
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
