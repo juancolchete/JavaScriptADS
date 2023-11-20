6a556468796c4121d8758b69f7a5986744e15660
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-11-19 21:29:51 -0300

    refactor: comparison with the inequality operator

--- basic-javascript/comparison-with-the-inequality-operator.js
+++ basic-javascript/comparison-with-the-inequality-operator.js
@@ -1,9 +1,9 @@
 // Setup
 function testNotEqual(val) {
-    if (val != 99) { // Change this line
-      return "Not Equal";
-    }
-    return "Equal";
+  if (val != 99) { // Change this line
+    return "Not Equal";
   }
-  
-  testNotEqual(10);
+  return "Equal";
+}
+
+testNotEqual(10);
