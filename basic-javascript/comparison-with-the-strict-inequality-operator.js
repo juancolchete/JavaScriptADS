73f6342c2b5c52bc9a516805e83b0ce947c500df
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-11-19 21:32:54 -0300

    refactor: comparison strict inequality operator

--- basic-javascript/comparison-with-the-strict-inequality-operator.js
+++ basic-javascript/comparison-with-the-strict-inequality-operator.js
@@ -1,9 +1,9 @@
 // Setup
 function testStrictNotEqual(val) {
-    if (val !== 17) { // Change this line
-      return "Not Equal";
-    }
-    return "Equal";
+  if (val !== 17) { // Change this line
+    return "Not Equal";
   }
-  
-  testStrictNotEqual(10);
+  return "Equal";
+}
+
+testStrictNotEqual(10);
