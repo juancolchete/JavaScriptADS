6842d8037a1e588349153e4acd86180230416a87
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-11-19 21:41:01 -0300

    refactor: comparison with the less than operator

--- basic-javascript/comparison-with-the-less-than-operator.js
+++ basic-javascript/comparison-with-the-less-than-operator.js
@@ -1,13 +1,13 @@
 function testLessThan(val) {
-    if (val < 25) {  // Change this line
-      return "Under 25";
-    }
-  
-    if (val < 55) {  // Change this line
-      return "Under 55";
-    }
-  
-    return "55 or Over";
+  if (val < 25) {  // Change this line
+    return "Under 25";
   }
-  
-  testLessThan(10);
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
