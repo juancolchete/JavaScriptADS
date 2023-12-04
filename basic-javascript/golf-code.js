ab97ada72509ddf7123bf82866601e74c0324e24
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-04 11:40:25 -0300

    refactor: golf code

--- basic-javascript/golf-code.js
+++ basic-javascript/golf-code.js
@@ -1,25 +1,23 @@
-var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
 function golfScore(par, strokes) {
   // Only change code below this line
-  var result=0;
   if(strokes == 1){
-    result = 0;
-  }else if(strokes <= par - 2){
-    result = 1;
-  }else if(strokes == par - 1){
-    result = 2;
+    return names[0];
+  }else if(strokes <= par -2){
+    return names[1];
+  }else if(strokes == par -1){
+    return names[2];
   }else if(strokes == par){
-    result = 3;
+    return names[3];
   }else if(strokes == par + 1){
-    result = 4;
+    return names[4];
   }else if(strokes == par + 2){
-    result = 5;
+    return names[5];
   }else{
-    result = 6;
+    return names[6];
   }
-
-  return names[result];
   // Only change code above this line
 }
 
-golfScore(4, 1);
+golfScore(5, 4);
