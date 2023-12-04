895fefc616484d38a481eda5046d9a4b876daa9f
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-04 12:11:03 -0300

    refactor: counting cards

--- basic-javascript/counting-cards.js
+++ basic-javascript/counting-cards.js
@@ -1,19 +1,30 @@
-var count = 0;
+let count = 0;
 
 function cc(card) {
   // Only change code below this line
-  var response  = "";
-  if(card <= 6){
-    count++;
-  }else if(!(card <= 9)){
-    count--;
+  
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
   }
+  let action = " Hold"
   if(count > 0){
-    response = count+" Bet" 
-  }else{
-    response = count+" Hold"
+    action = " Bet"
   }
-  return response;
+
+  return count + action;
   // Only change code above this line
 }
 
