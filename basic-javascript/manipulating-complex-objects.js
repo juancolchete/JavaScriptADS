5d18b20e487f02ddce4d75119010c665a54ea3ef
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 08:45:12 -0300

    refactor: manipulating complex objects

--- basic-javascript/manipulating-complex-objects.js
+++ basic-javascript/manipulating-complex-objects.js
@@ -1,23 +1,20 @@
 const myMusic = [
-    {
-      "artist": "Billy Joel",
-      "title": "Piano Man",
-      "release_year": 1973,
-      "formats": [
-        "CD",
-        "8T",
-        "LP"
-      ],
-      "gold": true
-    },
-    {
-      "artist": "Ednaldo Pereira",
-      "title": "Vale tudo vale nada",
-      "release_year": 2010,
-      "formats": [
-        "CD",
-        "Digital"
-      ],
-      "gold": true
-    }
-  ];
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push({
+  artist:"Missio",
+  title:"Vilain",
+  release_year:2023,
+  formats:["Digital","CD"]
+  })
