0c2aa3a054e5b094be6c0fb85ba3c403083bf980
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 09:05:40 -0300

    refactor: record collection

--- basic-javascript/record-collection.js
+++ basic-javascript/record-collection.js
@@ -1,36 +1,43 @@
 // Setup
 const recordCollection = {
-    2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
-    },
-    2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
-    },
-    1245: {
-      artist: 'Robert Palmer',
-      tracks: []
-    },
-    5439: {
-      albumTitle: 'ABBA Gold'
-    }
-  };
-  
-  // Only change code below this line
-  function updateRecords(records, id, prop, value) {
-    if (prop !== 'tracks' && value !== "") {
-      records[id][prop] = value;
-    } else if (prop === "tracks" && !records[id].hasOwnProperty("tracks")) {
-      records[id][prop] = [value];
-    } else if (prop === "tracks" && value !== "") {
-      records[id][prop].push(value);
-    } else if (value === "") {
-      delete records[id][prop];
-    }
-    return records;
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-  
-  updateRecords(recordCollection, 2548, "artist", "");
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(prop != "tracks"){
+    if(value == ""){
+      delete records[id][prop];
+    }else{
+      records[id][prop] = value;
+    }
+  }else{
+    if(value == ""){
+      delete delete records[id][prop];
+    }else{
+      if(records[id].hasOwnProperty(prop) == false){
+        records[id][prop] = [];
+      }
+      records[id][prop].push(value);
+    }
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
