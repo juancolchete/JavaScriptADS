525d4dc89bddb65b62d1a9248c40bdca6029e7d6
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 08:17:27 -0300

    refactor: access object properties bracket notation

--- basic-javascript/accessing-object-properties-with-bracket-notation.js
+++ basic-javascript/accessing-object-properties-with-bracket-notation.js
@@ -1,11 +1,10 @@
 // Setup
-var testObj = {
-    "an entree": "hamburger",
-    "my side": "veggies",
-    "the drink": "water"
-  };
-  
-  // Only change code below this line
-  
-  var entreeValue = testObj["an entree"];   // Change this line
-  var drinkValue = testObj["the drink"];    // Change this line
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
