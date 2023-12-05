095f2e9acf0f66890b89478de5dc12f075a38311
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 08:39:52 -0300

    refactor: testing objects for properties

--- basic-javascript/testing-objects-for-properties.js
+++ basic-javascript/testing-objects-for-properties.js
@@ -1,12 +1,9 @@
 function checkObj(obj, checkProp) {
-    // Only change code below this line
-    var keys = Object.keys(obj)
-    for(var i=0;i<keys.length;i++){
-      if(obj.hasOwnProperty(checkProp)){
-        return obj[checkProp];
-      }else{
-        return "Not Found";
-      }
-    }
-    // Only change code above this line
+  // Only change code below this line
+  if(obj.hasOwnProperty(checkProp) == true){
+    return obj[checkProp]
+  }else{
+    return "Not Found";
   }
+  // Only change code above this line
+}
