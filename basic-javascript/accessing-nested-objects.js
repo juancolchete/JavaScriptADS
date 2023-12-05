bf082bc0f60bb8293db45a802b8a7007f7d978ec
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 08:48:52 -0300

    refactor: accessing nested objects

--- basic-javascript/accessing-nested-objects.js
+++ basic-javascript/accessing-nested-objects.js
@@ -1,13 +1,13 @@
 const myStorage = {
-    "car": {
-      "inside": {
-        "glove box": "maps",
-        "passenger seat": "crumbs"
-       },
-      "outside": {
-        "trunk": "jack"
-      }
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
     }
-  };
-  
-  const gloveBoxContents = myStorage.car.inside["glove box"];
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
