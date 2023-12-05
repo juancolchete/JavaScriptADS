6570e11b674ac18c66bd01b8b177ba0e9fe8d4ef
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-05 08:35:54 -0300

    refactor: using objects for lookups

--- basic-javascript/using-objects-for-lookups.js
+++ basic-javascript/using-objects-for-lookups.js
@@ -1,19 +1,19 @@
 // Setup
 function phoneticLookup(val) {
-    let result = "";
-  
-    // Only change code below this line
-    var lookup = {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
       "alpha": "Adams",
-      "bravo":"Boston",
-      "charlie":"Chicago",
-      "delta":"Denver",
-      "echo":"Easy",
-      "foxtrot":"Frank"
-    }
-    result = lookup[val];
-    // Only change code above this line
-    return result;
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
   }
-  
-  phoneticLookup("charlie");
+  result = lookup[val]
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
