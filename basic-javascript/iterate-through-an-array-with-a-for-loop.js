2081385cbd2f6a3f591a804666ec18497858d595
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-06 08:19:06 -0300

    refactor: iterate through an array with a for loop

--- basic-javascript/iterate-through-an-array-with-a-for-loop.js
+++ basic-javascript/iterate-through-an-array-with-a-for-loop.js
@@ -1,7 +1,8 @@
 // Setup
-var total = 0;
 const myArr = [2, 3, 4, 5, 6];
-for(var i = 0;i<myArr.length;i++){
+let total = 0;
+for(let i=0;i<myArr.length;i++){
   total += myArr[i];
 }
 // Only change code below this line
+
