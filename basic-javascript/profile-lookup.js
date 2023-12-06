71acf6d0cab0b20da8079983482a189d806fe319
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-06 08:44:56 -0300

    refactor: profile lookup

--- basic-javascript/profile-lookup.js
+++ basic-javascript/profile-lookup.js
@@ -1,46 +1,49 @@
 // Setup
 const contacts = [
-    {
-      firstName: "Akira",
-      lastName: "Laine",
-      number: "0543236543",
-      likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-      firstName: "Harry",
-      lastName: "Potter",
-      number: "0994372684",
-      likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-      firstName: "Sherlock",
-      lastName: "Holmes",
-      number: "0487345643",
-      likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-      firstName: "Kristian",
-      lastName: "Vos",
-      number: "unknown",
-      likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-  ];
-  
-  function lookUpProfile(name, prop) {
-    // Only change code below this line
-    let propValue;
-    contacts.forEach((element)=>{
-      if(element.firstName == name){
-        return propValue = element[prop]
-      }
-    })
-    if(propValue == null && prop == "address"){
-      propValue = "No such property";
-    }else if(propValue == null && prop == "number" || prop == "potato"){
-      propValue = "No such contact";
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  let contact;
+  for(let i=0;i<contacts.length;i++){
+    if(contacts[i].firstName == name){
+      contact = contacts[i]
     }
-    return propValue;
-    // Only change code above this line
   }
-  
-  lookUpProfile("Sherlock", "likes");
+  if(contact != null){
+    if(contact.hasOwnProperty(prop) == true){
+      return contact[prop]
+    }else{
+      return "No such property"
+    }
+  }else{
+    return "No such contact";
+  }
+  // Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");
