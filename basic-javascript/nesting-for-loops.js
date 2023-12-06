9d557f43d595ba8b4dee7ae3112efb94c2278674
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-06 08:23:52 -0300

    refator: nesting for loops

--- basic-javascript/nesting-for-loops.js
+++ basic-javascript/nesting-for-loops.js
@@ -1,13 +1,13 @@
 function multiplyAll(arr) {
-    let product = 1;
-    // Only change code below this line
-    arr.forEach((item)=>{
-      item.forEach((item)=>{
-        product *= item;
-      })
-    })
-    // Only change code above this line
-    return product;
+  let product = 1;
+  // Only change code below this line
+  for(let i=0;i < arr.length; i++){
+    for (let j=0; j < arr[i].length;j++){
+      product *= arr[i][j]
+    }
   }
-  
-  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
