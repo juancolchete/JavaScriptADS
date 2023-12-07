1ebf5c9a62fd2638ecd1ab47da43af8ba48cb3a7
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-07 09:29:14 -0300

    refactor: use recursion to create a countdown

--- basic-javascript/use-recursion-to-create-a-countdown.js
+++ basic-javascript/use-recursion-to-create-a-countdown.js
@@ -1,17 +1,13 @@
 // Only change code below this line
-let maxNumber=0;
-let flag = true;
+let countArray = []
 function countdown(n){
-  if(flag){
-    flag = false
-    maxNumber = n;
-  }
-  if(n < 1){
+  if (n < 1) {
     return [];
-  }else{
+  } else {
     const countArray = countdown(n - 1);
-    countArray.push(maxNumber - n + 1);
+    countArray.unshift(n);
     return countArray;
   }
 }
 // Only change code above this line
+
