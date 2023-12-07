eb8c9ac9c06fb4174fd67e33918aaa52f18ee63c
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2023-12-07 09:04:24 -0300

    refator: generate random whole numbers in range

--- basic-javascript/generate-random-whole-numbers-within-a-range.js
+++ basic-javascript/generate-random-whole-numbers-within-a-range.js
@@ -1,5 +1,3 @@
 function randomRange(myMin, myMax) {
-    // Only change code below this line
-    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-    // Only change code above this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
 }
