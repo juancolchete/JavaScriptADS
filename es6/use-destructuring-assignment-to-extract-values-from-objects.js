4a7a93b8f5acd8fdf9f4c1531dd0c00fc2f7db3b
juancolchete <juanudkp@gmail.com>
2024-05-09 11:59:44 +0000

    refactor: desestructuring assign extract object

--- es6/use-destructuring-assignment-to-extract-values-from-objects.js
+++ es6/use-destructuring-assignment-to-extract-values-from-objects.js
@@ -1,11 +1,11 @@
 const HIGH_TEMPERATURES = {
-    yesterday: 75,
-    today: 77,
-    tomorrow: 80
-  };
-  
-  // Only change code below this line
-  
-  const { today, tomorrow } = HIGH_TEMPERATURES;
-  
-  // Only change code above this line
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
+
+// Only change code above this line
