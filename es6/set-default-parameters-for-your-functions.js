9aad9c10923de010ff954102648cdf82d816deb2
juancolchete <juanudkp@gmail.com>
2024-05-09 11:47:52 +0000

    refactor: set defualt parameters for functions

--- es6/set-default-parameters-for-your-functions.js
+++ es6/set-default-parameters-for-your-functions.js
@@ -1,3 +1,3 @@
 // Only change code below this line
-const increment = (number, value=1) => number + value;
+const increment = (number = 1, value = 1) => number + value;
 // Only change code above this line
