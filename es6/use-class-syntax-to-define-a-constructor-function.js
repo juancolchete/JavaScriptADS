954405f86813f978dcf43972a929856b31f622fd
juancolchete <juanudkp@gmail.com>
2024-06-03 11:50:31 +0000

    refactor: use class syntax to define a constructor

--- es6/use-class-syntax-to-define-a-constructor-function.js
+++ es6/use-class-syntax-to-define-a-constructor-function.js
@@ -1,8 +1,8 @@
 // Only change code below this line
-class Vegetable {
-    constructor(name) {
-        this.name = name;
-    }
+class Vegetable{
+  constructor(name){
+    this.name = name
+  }
 }
 // Only change code above this line
 
