f469fe6b9e71a41c1e2645b702a82880bb8b83a5
juancolchete <juanudkp@gmail.com>
2024-06-07 11:30:27 +0000

    refactor: export to share a code block

--- es6/use-export-to-share-a-code-block.js
+++ es6/use-export-to-share-a-code-block.js
@@ -1,9 +1,9 @@
 const uppercaseString = (string) => {
-    return string.toUpperCase();
-  }
-  
-  const lowercaseString = (string) => {
-    return string.toLowerCase()
-  }
-  
-  export {uppercaseString,lowercaseString}
+  return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+  return string.toLowerCase()
+}
+
+export {uppercaseString,lowercaseString}
