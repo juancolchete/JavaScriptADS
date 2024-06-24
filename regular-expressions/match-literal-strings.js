1ebcc22d6208e7949f33531b4227b4d2ffcc4d64
juancolchete <juanudkp@gmail.com>
2024-06-24 18:07:29 +0000

    refactor: match literal strings

--- regular-expressions/match-literal-strings.js
+++ regular-expressions/match-literal-strings.js
@@ -1,3 +1,3 @@
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
-let waldoRegex = /Waldo/; // Change this line
-let result = waldoRegex.test(waldoIsHiding);
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
