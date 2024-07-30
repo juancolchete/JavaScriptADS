aae5dd19e544e2bc03ac370238fdc887273b745c
juancolchete <juanudkp@gmail.com>
2024-07-30 14:55:50 +0000

    refactor: match characters that occur zero or more\

--- regular-expressions/match-characters-that-occur-zero-or-more-times.js
+++ regular-expressions/match-characters-that-occur-zero-or-more-times.js
@@ -1,5 +1,6 @@
 // Only change code below this line
-let chewieRegex = /^[a*]{1,}/gi; // Change this line
+let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!"
+let chewieRegex = /Aa*/g; // Change this line
 // Only change code above this line
 
 let result = chewieQuote.match(chewieRegex);
